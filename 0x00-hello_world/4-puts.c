d6f13538e9a68ba9f165b9e6b0ba5c25e37572e8
KoolC2 <funmi1234check@gmail.com>
2022-03-13 21:22:13 +0000

    my scripts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-* Start
-* 
-* Return: Always 0
-*/
+ * Start
+ *
+ *Return: Always 0
+ */
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
