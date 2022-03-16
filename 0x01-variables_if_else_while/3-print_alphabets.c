f3c747725e51809cbb848c19d9077b95ad0b6b16
KoolC2 <funmi1234check@gmail.com>
2022-03-16 22:30:03 +0000

    new project

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-	int n;
+	char low;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n==0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
+	for (low = 'a; low<= 'z'; low++)
+	{
+		putchar(low);
+		}
+	for (low = 'A'; low <= 'Z'; low++)
+	{
+		putchar(low);
+		}	
+		putchar('\n')
 
 	return (0);
 }
