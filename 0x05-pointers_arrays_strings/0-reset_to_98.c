8a1c7c7493984461f65319663f8ee1ed05c1cb17
KoolC2 <funmi1234check@gmail.com>
2022-03-23 22:56:04 +0000

    Rebound

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to-98 updates the value it points
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (0);
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Learning_pointers-arrays
