75fdc51ba2c2f4f64fad3c49e3a2ba799360093d
KoolC2 <funmi1234check@gmail.com>
2022-03-24 00:17:06 +0000

    Rebound

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,17 @@
-#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap values of integer
+ * @a: first interge
+ * @b: second integer
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	c = *b;
+
+}
+
