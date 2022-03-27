b0315d91af85723db2c9109fd789cef4f314e53c
KoolC2 <funmi1234check@gmail.com>
2022-03-27 22:47:08 +0000

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints princess followed by a newline
+ *
+ * Return: returns 0
+ *
+ */
+
+int main(void)
+
+{
+
+	char princess[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+{
+	_putchar(princess[i]);
+}
+_putchar('\n');
+return (0);
+}
