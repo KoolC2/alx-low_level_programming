8d52b864be62effe5d51f21d1cef4ebc5f4a8da6
KoolC2 <funmi1234check@gmail.com>
2022-03-28 00:04:48 +0000

    nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,19 @@
 #include <stdio.h>
 
 /**
- * main - 
- * Description - 
+ * print_alphabet - prints all alphabets in lowercase
+ *
+ * Description: Sorted
  * Return: Always 0
  */
 
+void print_alphabet(void)
+{
+	char ch = 'a';
 
-
-
-
-return (0);
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
