f34e5b90fff271bb039d6031de2c2514f41687f3
KoolC2 <funmi1234check@gmail.com>
2022-03-28 20:42:53 +0000

    pointer to more pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_memset - fills memory
+ *@s: input string
+ *@a: input char
+ *@b: input size
+ *
+ * Description: fill memory with a constant byte
+ * Return: returns pointer to memory areas
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+
+
+
+return ();
+}
