f409ced43eda4d8f13451861d2252ff8b1627ebf
KoolC2 <funmi1234check@gmail.com>
2022-03-31 23:43:50 +0000

    pointer to more pointers

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,11 +2,34 @@
 #include "main.h"
 
 /**
- * _puts_recursion - the function prints string
- * @ s: inputs string
+ * _sqrt - returns sqrt
+ * @n: int parameters
+ * @i: value to be tested
  *
- * Description: Prints a string folowed by a new line.
+ * Description: Returns natural square root of a number.
  * Return: Always (0)
  */
 
-void _puts_recursion(char *s)
+int _sqrt(int n, int i)
+{
+	if (n < 0)
+		return (-1);
+	if ((i * i) > n)
+		return (-1);
+	if ((i * i) == n)
+		return (i);
+
+	return (_sqrt(n, i + 1));
+}
+/**
+ * _sqrt_recursion - returns square root
+ *
+ * @n: number to find sqrt
+ *
+ * Return: sqrt, (-1) no natural sqrt 
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}	}
