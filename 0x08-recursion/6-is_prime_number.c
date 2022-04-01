c7f34ff22e46cf5856c337a32072ddd15b10149f
KoolC2 <funmi1234check@gmail.com>
2022-04-01 22:30:15 +0000

    pointer to more pointers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ *_prime - return 1 if prime no
+ *@a: input 1
+ *@b: input 2
+ *
+ * Description: Print 1 if prime no
+ * Return: print 1 if prime, print 0 if else
+ */
+
+int _prime(int a, int b)
+{
+	if (b < 2 || b % a == 0)
+
+		return (0);
+
+	else if (a > b / 2)
+
+		return (1);
+	else
+		return (_prime(a + 1, b));
+}
 
 /**
  * is_prime_number - prints prime number
@@ -16,4 +37,3 @@ int is_prime_number(int n)
 
 	return (_prime(2, n));
 }
-
