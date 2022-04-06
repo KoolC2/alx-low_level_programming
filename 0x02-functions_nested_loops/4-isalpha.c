b302b4db2961c27b65d74172c38ebba3ce8822b8
KoolC2 <funmi1234check@gmail.com>
2022-04-06 23:07:08 +0000

    making arugments

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,24 @@
 #include <stdio.h>
 
 /**
- * main - 
- * Description - 
- * Return: Always 0
+ * _isalpha - Entry point
+ * @c: the integer value it receives
+ * Description: checks if alphabet
+ * Return: 1 if true. 0 if false
  */
 
-
-
-
-
-return (0);
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
