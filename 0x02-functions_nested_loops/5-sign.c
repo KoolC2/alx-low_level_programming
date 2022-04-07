d4ff1194f5230715605aa280b43f81d447eefbbf
KoolC2 <funmi1234check@gmail.com>
2022-04-07 00:16:45 +0000

    making arugments

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,35 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - 
- * Description - 
- * Return: Always 0
+ * print_sign - Checks if input number is +, 0 or -
+ * @n: number
+ * Return: 1 and prints + if n is > zero, 0 and prints 0 if n is zero,
+ * Returns -1 and prints - if n is < zero
  */
+
+int print_sign(int n)
+{
+	if (n < 0)
+
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+	return (n);
+}
+
+
 
 
 
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * main - 
- * Description - 
- * Return: Always 0
+/*
+ * _abs - computes value of integer
+ *
+ * @n: number
+ * Descriptin: Comoutes integer
+ * Return: computes  value
  */
 
-
-
-
-
-return (0);
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
