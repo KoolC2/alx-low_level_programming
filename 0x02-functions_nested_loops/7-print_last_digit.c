7fa25ee94ceca52f8d16325c5d89e2d510d549e1
KoolC2 <funmi1234check@gmail.com>
2022-04-08 22:14:47 +0000

    making arugments

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,24 @@
 #include <stdio.h>
 
 /**
- * main - 
- * Description - 
- * Return: Always 0
+ * print_last_digit - Print the last digit of a number
+ * @l: An integer input
+ * Description: This function prints the last digit of a number
+ * Return: last digit of number
  */
+int print_last_digit(int l)
+{
+	int n;
 
+	if (l < 0)
+	{
+		n = -1 * (l % 10);
+	}
+	else
+	{
+		n = l % 10;
+		_putchar((n % 10) + '0');
+	}
 
-
-
-
-return (0);
+	return (n % 10);
+}
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,13 +2,24 @@
 #include <stdio.h>
 
 /**
- * main - 
- * Description - 
- * Return: Always 0
+ * jack_bauer - prints everys minutes jack_bauer
+ * Description: function prints every minute in one day
+ * Return: 0
  */
+void jack_bauer(void)
+{
+	int H, M;
 
-
-
-
-
-return (0);
+	for (H = 0; H < 24; H++)
+	{
+		for (M = 0; M < 60; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n')
+		}
+	}
+}
