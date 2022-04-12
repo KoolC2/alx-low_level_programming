eff4fa4ede88cdf08a9834e376120381319d61c7
KoolC2 <funmi1234check@gmail.com>
2022-04-12 23:48:58 +0000

    Macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file complied from
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
+
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,15 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 4-sum.h
+ * Description: Header file that defines a function-like macro SUM(x,y)
+ * @x: number
+ * @y: number
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
+
+
