40684862a1f7d105d0a5bbe1442109ab1b92c57a
KoolC2 <funmi1234check@gmail.com>
2022-04-13 22:05:00 +0000

    structures

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#nclude "dog.h"
 #include <stdlib.h>
 
 /**
@@ -8,24 +7,7 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-
-	free(d->owner);
-	free(d->name);
-	free(d);
-}
-
-include <stdlib.h>
-
-/**
- * free_dog - Frees dogs.
- * @d: The dog to be freed.
- */
-void free_dog(dog_t *d)
-{
-	if (d == NULL)
-		return;
+	if (d)
 
 	free(d->owner);
 	free(d->name);
